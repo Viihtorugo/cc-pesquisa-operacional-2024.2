844ce3a5ed3d60e0bf5d0b63a3a3e145cba740bd
Viihtorugo <victor-torugo@hotmail.com>
2025-04-18 18:39:20 -0300

    feat(Problema Escalonamento de horários): Add the model

--- lista/2.7.cpp
+++ lista/2.7.cpp
@@ -9,69 +9,43 @@ int main()
     try 
     {
         IloModel model(env);
-        
-        vector <string> t;
-        t.push_back("Milho");
-        t.push_back("Arroz");
-        t.push_back("Feijão");
 
-        IloArray<IloNumVarArray> x(env, 3);
-        for (int i = 0; i < 3; ++i) {
-            x[i] = IloNumVarArray(env, 3);
-            for (int j = 0; j < 3; ++j) {
-                string var_name = "x_fazenda" + to_string(i) + "_" + t[j];
-                x[i][j] = IloNumVar(env, 0, IloInfinity, var_name.c_str());
-            }
+        vector<int> d = {5, 6, 4, 7, 3, 4, 6};
+        
+        //Variaveis de decisão
+        IloNumVarArray x(env, 7);
+        for (int i = 0; i < 7; ++i) 
+        {
+            string var_name = "x_" + to_string(i + 1);
+            x[i] = IloIntVar(env, 0, IloIntMax, var_name.c_str());
         }
 
+        //Função Objetivo
         IloExpr obj(env);
         
-        vector <int> l;
-        l.push_back(5000);
-        l.push_back(4000);
-        l.push_back(1800);
+        for (int i = 0; i < 7; ++i)
+            obj += x[i];
 
-        for (int i = 0; i < 3; ++i)
-            obj += ((x[i][0] + x[i][1] + x[i][2]) * l[i]);
-
-        model.add(IloMaximize(env, obj));
-
-        //restrição do tamanho da area da fazenda
-        vector <double> area_fazenda;
-        area_fazenda.push_back(400);
-        area_fazenda.push_back(600);
-        area_fazenda.push_back(350);
-
-        //restrição da qtd de agua de cada fazenda
-        vector <double> total_agua_area;
-        total_agua_area.push_back(1800);
-        total_agua_area.push_back(2200);
-        total_agua_area.push_back(950);
-
-        vector <double> agua_area;
-        agua_area.push_back(5.5);
-        agua_area.push_back(4);
-        agua_area.push_back(3.5);
+        model.add(IloMinimize(env, obj));
         
-
-        for (int i = 0; i < 3; ++i)
+        //Restrições
+        for (int i = 0; i < 7; ++i)
         {
-            model.add(x[i][0] + x[i][1] + x[i][2] <= area_fazenda[i]);
-            model.add(agua_area[0] * x[i][0] + agua_area[1] * x[i][1] + agua_area[2] * x[i][2] <= total_agua_area[i]);
-        }
-        
-        //Restrição da area max de cada cultura
-        vector <double> area_max;
-        area_max.push_back(660);
-        area_max.push_back(880);
-        area_max.push_back(400);
+            IloExpr sum(env);
 
-        for (int i = 0; i < 3; ++i)
-            model.add(x[0][i] + x[1][i] + x[2][i] <= area_max[i]);
+            for (int j = 4; j >= 0; j--)
+                sum += x[ ((i - j) + 7) % 7 ];
+            
+            model.add(sum >= d[i]);
+        }
 
         IloCplex cplex(model);
         cplex.solve();
 
+        cout << "Número mínimo de enfermeira: " << cplex.getObjValue() << endl << endl;
+
+        for (int i = 0; i < 7; ++i)
+            cout << cplex.getValue(x[i]) << " enfermeira(s) comeraçaram no dia " << (i + 1) << endl;
 
     } 
     catch (IloException& e) 

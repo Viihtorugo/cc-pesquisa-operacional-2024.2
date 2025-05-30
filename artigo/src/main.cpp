9c3bee5d66ded3882ec34ae452d2b474a8310c18
Viihtorugo <victor-torugo@hotmail.com>
2025-05-30 08:04:37 -0300

    feat(simulation): Add the simulation

--- artigo/src/main.cpp
+++ artigo/src/main.cpp
@@ -3,6 +3,22 @@
 
 using namespace std;
 
+vector<vector<int>> floyd_warshall(int n, const vector<vector<int>> &adj_matrix) {
+    vector<vector<int>> dist = adj_matrix;
+    const int INF = numeric_limits<int>::infinity();
+
+    for (int k = 0; k < n; ++k) {
+        for (int i = 0; i < n; ++i) {
+            for (int j = 0; j < n; ++j) {
+                if (dist[i][k] != INF && dist[k][j] != INF) {
+                    dist[i][j] = min(dist[i][j], dist[i][k] + dist[k][j]);
+                }
+            }
+        }
+    }
+    return dist;
+}
+
 int main() {
 
     IloEnv env;
@@ -15,11 +31,12 @@ int main() {
         //file_data.open("src/input/data1.txt");
         file_data.open("src/input/data2.txt");
 
-        int n, m;
+        int n, m, N = 350;
         file_data >> n >> m;
             
         //vector <pair<int, int>> edges;
         vector <vector <int>> g (n, vector <int> ());
+        vector <vector <int>> adj_matrix (n, vector <int> (n));
         vector <int> num_streets;
         
         if (file_data.is_open()) 
@@ -34,13 +51,14 @@ int main() {
 
             for (int i = 0; i < m; i++)
             {
-                int u, v;
+                int u, v, w;
 
-                file_data >> u >> v;
+                file_data >> u >> v >> w;
 
                 g[u].push_back(v);
                 g[v].push_back(u);
-                cout << "index: " << i << endl;
+                adj_matrix[u][v] = v;
+                adj_matrix[v][u] = w;
             }
         }
         else
@@ -48,10 +66,22 @@ int main() {
             cout << "Erro ao abrir o arquivo ou arquivo não existe!\n";
             exit(1);
         }
+
+        vector <vector <int>> dist = floyd_warshall(n, adj_matrix);
+
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < n; j++)
+            {
+                cout << dist[i][j] << " ";
+            }
+
+            cout << "\n";
+        }
+
         IloModel model(env);
 
         //Variaveis de decisão
-
         IloNumVarArray x(env, n);
 
         for (int i = 0; i < n; ++i)
@@ -75,8 +105,9 @@ int main() {
             sum += x[i];
 
             for (auto v: g[i])
-                sum += x[v];
-            
+                if (dist[i][v] <= N)
+                    sum += x[v];
+
             model.add(sum >= 1);
         }
 
@@ -100,6 +131,7 @@ int main() {
 
         if (file_output.is_open()) 
         {
+            
             cout << "Tamanho mínimo da cobertura: " << cplex.getObjValue() << endl;
             cout << "Vértices na cobertura: ";
             for (int i = 0; i < n; ++i) 
